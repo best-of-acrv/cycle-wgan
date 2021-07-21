65730b54cbf05e252dfd15465f61bdbe81032f98
Ben Talbot <b.talbot@qut.edu.au>
2021-07-22 09:28:38 +1000

    Create a starting setup.py for pip package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='cycle-wgan',
+    version='0.9.0',
+    author='Ben Talbot',
+    author_email='b.talbot@qut.edu.au',
+    url='https://github.com/best-of-acrv/cycle-wgan',
+    description=
+    'Generalised zero-shot learning (GZSL) semantic classification using '
+    'multi-modal cycle-consistent feature generation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['acrv_datasets'],
+    entry_points={'console_scripts': ['cycle-wgan=cycle_wgan.__main__:main']},
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ))
