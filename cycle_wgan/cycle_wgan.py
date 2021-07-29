cddc77b3bb5c334753f59eda1de3e4e6ebebcf39
Ben Talbot <b.talbot@qut.edu.au>
2021-07-30 06:47:23 +1000

    Create class fn sigs with first guess of args

--- cycle_wgan/cycle_wgan.py
+++ cycle_wgan/cycle_wgan.py
@@ -1,4 +1,25 @@
 class CycleWgan(object):
+    DATASETS = ['awa1', 'cub', 'flo', 'sun']
 
-    def __init__(self):
+    def __init__(
+        self,
+        *,
+        config=None,
+        cpu=False,
+        gpu_id=0,
+        model_seed=0,
+    ):
         print("Created instance...")
+
+    def evaluate(self,
+                 dataset_name,
+                 *,
+                 dataset_dir=None,
+                 output_directory='./eval_output'):
+        pass
+
+    def predict(self, *, image=None, image_file=None, output_file=None):
+        pass
+
+    def train(self, dataset_name, *, dataset_dir=None):
+        pass
