0ad350e9ef52b92e2d24b579719ccdcf3f6f4c80
Ben Talbot <b.talbot@qut.edu.au>
2021-07-30 14:23:32 +1000

    Migrate training process to new structure

--- cycle_wgan/cycle_wgan.py
+++ cycle_wgan/cycle_wgan.py
@@ -5,12 +5,15 @@ import os
 import pkg_resources
 import torch
 
-from .helpers import create_dir
-from .utils.datasets import load
+from . import models
+from . import helpers
+from .utils.datasets import augment_dataset, load
 
 
 class CycleWgan(object):
+    AUGMENTATION_METHODS = ['none', 'replace', 'merge']
     DATASETS = ['awa1', 'cub', 'flo', 'sun']
+    DOMAINS = ['unseen', 'seen', 'unseen seen']
 
     def __init__(
         self,
@@ -37,15 +40,15 @@ class CycleWgan(object):
         if not self.cpu and torch.cuda.is_available():
             os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
             os.environ['CUDA_VISIBLE_DEVICES'] = str(self.gpu_id)
-            torch.manual_seed(self.gpu_id)
-            torch.cuda.manual_seed(self.gpu_id)
+            torch.manual_seed(self.model_seed)
+            torch.cuda.manual_seed(self.model_seed)
         elif not torch.cuda.is_available():
             raise RuntimeWarning('PyTorch could not find CUDA, using CPU ...')
         else:
             raise RuntimeWarning(
                 'PyTorch is using CPU as requested by cpu flag.')
 
-        # Load the model
+        # Load the models
         # TODO
 
     def evaluate(self, *, output_directory='./eval_output'):
@@ -54,19 +57,63 @@ class CycleWgan(object):
     def predict(self, *, image=None, image_file=None, output_file=None):
         pass
 
-    def train(self, *, output_directory='./train_output', train_gan=True):
+    def train(self,
+              *,
+              augmentation_method='none',
+              domain='unseen seen',
+              generate_fake_data=True,
+              number_features=[1200, 300],
+              output_directory=None,
+              train_gan=True,
+              train_gzsl=True):
         # Load in the dataset
         dataset, knn = _load_dataset(self.config['dataset'],
                                      self.config.get('data_dir', None))
+        device = torch.device('cpu') if self.cpu else None
 
-        # Create a unique working directory for the output
-        root = os.path.join(output_directory,
-                            datetime.now().strftime(r'%Y%m%d_%H%M%S'))
-        create_dir(root)
+        # Sanitise & validate arguments
+        aug_method = _sanitise_arg(augmentation_method, 'augmentation_method',
+                                   CycleWgan.AUGMENTATION_METHODS)
+        domain = _sanitise_arg(domain, 'domain', CycleWgan.DOMAINS)
+        if not any([train_gan, generate_fake_data, train_gzsl]):
+            raise ValueError("Must select at least one of 'train_gan', "
+                             "'generate_fake_data', or 'train_gzsl'")
 
-        # Perform GAN training if requested
+        # Create a unique working directory for the output if none was
+        # explicitly provided
+        if output_directory == None:
+            output_directory = os.path.join(
+                './train_output',
+                datetime.now().strftime(r'%Y%m%d_%H%M%S'))
+            helpers.create_dir(output_directory)
+
+        # Train GAN if requested
+        gan = None
+        gan_dir = os.path.join(output_directory, 'gan')
         if train_gan:
-            gan = 0
+            gan = helpers.train_gan(device, gan_dir, self.config['GAN'],
+                                    dataset.train)
+
+        # Generate a dataset of fake visual samples if requested
+        fake_file = os.path.join(output_directory, 'generated_data', 'data.h5')
+        if generate_fake_data:
+            if gan is None:
+                gan = helpers.setup_model(models.GAN, device, gan_dir,
+                                          self.config['GAN'])
+            helpers.generate_fake_data(gan, knn, fake_file, domain,
+                                       number_features)
+
+        # Apply the selected augmentation method in adding fakes to dataset
+        if augmentation_method != CycleWgan.AUGMENTATION_METHODS[0]:
+            dataset = augment_dataset(dataset, fake_file, augmentation_method)
+
+        # Train GZSL classifier if requested
+        classifier = None
+        classifier_dir = os.path.join(output_directory, 'gzsl_classifier')
+        if train_gzsl:
+            classifier = helpers.train_gzsl_classifier(
+                device, classifier_dir, self.config['GZSL_classifier'],
+                dataset.train)
 
 
 def _load_dataset(dataset_name, dataset_dir=None, quiet=False):
