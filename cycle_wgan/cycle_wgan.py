9c83c76e31d4b0a7f1aae6cc98711b5cb1353402
Ben Talbot <b.talbot@qut.edu.au>
2021-07-30 11:18:23 +1000

    Migrate old run.py functions from to new structure

--- cycle_wgan/cycle_wgan.py
+++ cycle_wgan/cycle_wgan.py
@@ -1,8 +1,11 @@
 import acrv_datasets
+from datetime import datetime
 import json
 import os
 import pkg_resources
 import torch
+
+from .utils.datasets import load
 
 
 class CycleWgan(object):
@@ -50,10 +53,19 @@ class CycleWgan(object):
     def predict(self, *, image=None, image_file=None, output_file=None):
         pass
 
-    def train(self):
+    def train(self, *, output_directory='./train_output', train_gan=True):
         # Load in the dataset
         dataset, knn = _load_dataset(self.config['dataset'],
                                      self.config.get('data_dir', None))
+
+        # Create a unique working directory for the output
+        root = os.path.join(output_directory,
+                            datetime.now().strftime(r'%Y%m%d_%H%M%S'))
+        os.makedirs(root)
+
+        # Perform GAN training if requested
+        if train_gan:
+            gan = 0
 
 
 def _load_dataset(dataset_name, dataset_dir=None, quiet=False):
@@ -64,6 +76,9 @@ def _load_dataset(dataset_name, dataset_dir=None, quiet=False):
         dataset_dir = acrv_datasets.get_datasets(dataset_name)
     if not quiet:
         print("Using 'data_dir': %s" % dataset_dir)
+
+    # Return dataset and k-nearest neighbours from the dataset_dir
+    return load(dataset_dir)
 
 
 def _sanitise_arg(value, name, supported_list):
---
+++ cycle_wgan/helpers.py
@@ -0,0 +1,98 @@
+import os
+from sklearn.model_selection import train_test_split
+
+from .utils import loaders
+
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def generate_fake_data(gan, knn, aug_file, domain, num_features):
+    #Set-up directory
+    create_dir(str(Path(aug_file).parent))
+
+    #Used trained generator to synthesis fake visual samples - save to aug_file
+    gan.generator.generate_dataset(aug_file, knn, domain, num_features)
+
+
+def get_data_as_dict(x=None, y=None, a=None):
+    return {'x': x, 'y': y, 'a': a}
+
+
+def get_split_datasets(dataset, val_size=0.1):
+
+    #Split data into training/validation (1-val_size/val_size)
+    split = train_test_split(dataset.X,
+                             dataset.Y,
+                             dataset.A.continuous,
+                             test_size=val_size,
+                             random_state=42)
+
+    #Create dataset objects for both training and validation splits
+    train_data = get_data_as_dict(split[0], split[2], split[4])
+    train_dataset = loaders.LoaderH5(train_data)
+    val_data = get_data_as_dict(split[1], split[3], split[5])
+    val_dataset = loaders.LoaderH5(val_data)
+
+    return train_dataset, val_dataset
+
+
+def harmonic_mean(a, b):
+    return 2 * a * b / (a + b)
+
+
+def setup_model(model, device, model_dir, opts):
+    create_dir(model_dir)
+    return model(device, model_dir, opts)
+
+
+def train_gan(device, gan_dir, opts, dataset):
+
+    #Set-up directory and create GAN
+    gan = setup_model(models.GAN, device, gan_dir, opts)
+
+    #Get train/val datasets
+    train_dataset, val_dataset = get_split_datasets(dataset)
+
+    #Train GAN
+    gan.train(train_dataset, val_dataset)
+
+    return gan
+
+
+def train_gzsl_classifier(device, classifier_dir, opts, dataset):
+
+    #Set-up directory and create GZSL classifier
+    gzsl_classifier = setup_model(models.Classifier, device, classifier_dir,
+                                  opts)
+
+    #Get train/val datasets
+    train_dataset, val_dataset = get_split_datasets(dataset)
+
+    #Train a classifier for GZSL on the provided dataset
+    gzsl_classifier.train(train_dataset,
+                          val_dataset,
+                          batch_size=opts['batch_size'])
+
+    return gzsl_classifier
+
+
+def test_gzsl_classifier(gzsl_classifier, opts, dataset, knn):
+
+    #Get seen/unseen test data
+    test_dataset_seen = get_split_datasets(dataset.seen, val_size=0)[0]
+    test_dataset_unseen = get_split_datasets(dataset.unseen, val_size=0)[0]
+
+    #Per-class accuracies
+    batch_size = opts['batch_size'] if 'batch_size' in opts else 64
+    seen_acc = gzsl_classifier.class_accuracy(test_dataset_seen, batch_size,
+                                              knn.openval.ids - 1)
+    unseen_acc = gzsl_classifier.class_accuracy(test_dataset_unseen,
+                                                batch_size, knn.zsl.ids - 1)
+
+    print("Y(U) = {0:.4f}, Y(S) = {1:.4f}, H = {2:.4f}, ".format(
+        unseen_acc, seen_acc, harmonic_mean(seen_acc, unseen_acc)))
+
+    return unseen_acc, seen_acc
